2d8c8f982efc14040d4432318c5aec62a2df74e1
Minyeob <nmy2511@gmail.com>
2017-03-21 23:00:01 +0900

    excel_write - 건물까지

--- ibk/forms.py
+++ ibk/forms.py
@@ -38,8 +38,50 @@ class ExcelForm(forms.Form):
 
     #본건현황
 
+    address=forms.CharField(required=False) #Address
+    property_category=forms.CharField(required=False)   #Property category
+    usage=forms.CharField(required=False)   #용도지역
+    land_category=forms.CharField(required=False)   #지목
+    state=forms.CharField(required=False)   #이용상황
+    land_price_m=forms.CharField(required=False)    #개별공시지가 m
+    land_price_py=forms.CharField(required=False)   #개별공시지가 py
+    land_size_m=forms.CharField(required=False) #전체토지면적 m
+    land_size_py=forms.CharField(required=False)   #전체토지면적 py
+    security_size_m=forms.CharField(required=False) #담보면적 m
+    security_size_py=forms.CharField(required=False)    #담보면적 py
+    structure=forms.CharField(required=False)   #건물 구조
+    permission_date=forms.CharField(required=False) #사용승인일
+    floor_usage=forms.CharField(required=False) #층별 용도
+    exclusive_rate=forms.CharField(required=False)  #전용율
+    exclusive_area_m=forms.CharField(required=False)    #전유면적 m
+    exclusive_area_py=forms.CharField(required=False)   #전유면적 py
+    contract_area_m=forms.CharField(required=False) #계약면적 m
+    contract_area_py=forms.CharField(required=False)    #계약면적 py
 
     #건물
-    ho=forms.CharField(required=False)
+
+    building_label=forms.CharField(required=False)
+    building_ho=forms.CharField(required=False)
+    building_exclusive_m=forms.CharField(required=False)
+    building_exclusive_py=forms.CharField(required=False)
+    bulding_contract_m=forms.CharField(required=False)
+    building_contract_py=forms.CharField(required=False)
+    building_right_m=forms.CharField(required=False)
+    building_right_py=forms.CharField(required=False)
+    building_ratio=forms.CharField(required=False)
+    building_auction_price=forms.CharField(required=False)
+    building_auction_exclusive=forms.CharField(required=False)
+    building_auction_contract=forms.CharField(required=False)
+    building_auction_ratio=forms.CharField(required=False)
+    building_market_price = forms.CharField(required=False)
+    building_market_exclusive = forms.CharField(required=False)
+    building_market_contract = forms.CharField(required=False)
+    bulding_market_ma=forms.CharField(required=False)
+    building_estimated_price = forms.CharField(required=False)
+    building_estimated_exclusive = forms.CharField(required=False)
+    building_estimated_contract = forms.CharField(required=False)
+    bulding_estimated_ea = forms.CharField(required=False)
+    bulding_estimated_em = forms.CharField(required=False)
+
 
 
--- ibk/views.py
+++ ibk/views.py
@@ -65,10 +65,11 @@ def show_normal_report(request, code):
     setup_price=excel_handling().get_cpma(workbook, loc) #설정액
     address=excel_handling().get_address(workbook, loc, code)    #Address
     category=excel_handling().get_property_category(workbook, loc)  #Property category
+    label=['가','나','다','라','마','바','사','아','자','차','카','타','파','하']
     ho=excel_handling().get_ho(workbook, code)  #건물의 호들
     liensize_improvement=excel_handling().get_liensize_improvement(workbook, code)  #전유면적 - 일단 엑셀에서 건물면적을 꺼내서 구함
     landsize=excel_handling().get_landsize(workbook, code)  #대지권 면적
-    building=zip(ho, liensize_improvement, landsize)
+    building=zip(label, ho, liensize_improvement, landsize)
     utensil=excel_handling().get_utensil(workbook, loc) #기계기구의 숫자
 
     address_code=excel_handling().get_address_code(workbook,loc)
@@ -89,16 +90,86 @@ def download(request):
         form = ExcelForm(request.POST)
         #form은 valid 검사를 하고 나서야 cleaned_data를 가진다
         if form.is_valid():
-            program=form.cleaned_data['program']
-            opb=form.cleaned_data['opb']
-            interest=form.cleaned_data['interest']
-            property_control_no=form.cleaned_data['property_control_no']
-            setup_price=form.cleaned_data['setup_price']
-            user=form.cleaned_data['user']
-            ho=request.POST.getlist('ho[]')
-            submission_date=form.cleaned_data['submission_date']
+            #dictionary 형태로 선언하기 위해서는 중괄호 {} 로 선언해야 한다
+            user_input={}
+            #결과요약
+            user_input['program'] = form.cleaned_data['program']
+            user_input['opb'] = form.cleaned_data['opb']
+            user_input['interest'] = form.cleaned_data['interest']
+            user_input['property_control_no'] = form.cleaned_data['property_control_no']
+            user_input['setup_price'] = form.cleaned_data['setup_price']
+            user_input['user'] = form.cleaned_data['user']
+            user_input['user_phone'] = form.cleaned_data['user_phone']  # 담당자 연락처
+            user_input['credit_amount'] = form.cleaned_data['credit_amount'] # 총 채권액
+            user_input['borrow_name'] = form.cleaned_data['borrow_name']  # borrow name
+            user_input['law_price'] = form.cleaned_data['law_price']  # 법사가
+            user_input['market_predict'] = form.cleaned_data['market_predict']  # 시장전망
+            user_input['market_price'] = form.cleaned_data['market_price']  # 시장가
+            user_input['law_price_comp1'] = form.cleaned_data['law_price_comp1']  # 법사가 대비 1
+            user_input['market_price_comp1'] = form.cleaned_data['market_price_comp1']    # 시장가 대비 1
+            user_input['opb_comp1'] = form.cleaned_data['opb_comp1']  # opb 대비 1
+            user_input['court'] = form.cleaned_data['court']  # 관할법원
+            user_input['bid'] = form.cleaned_data['bid']  # 낙찰가
+            user_input['law_price_comp2'] = form.cleaned_data['law_price_comp2']  # 법사가 대비 2
+            user_input['market_price_comp2'] = form.cleaned_data['market_price_comp2']  # 시장가 대비 2
+            user_input['opb_comp2'] = form.cleaned_data['opb_comp2']  # opb 대비 2
+            user_input['case'] = form.cleaned_data['case']  # 사건번호
+            user_input['avg_bid'] = form.cleaned_data['avg_bid']  # 평균낙찰가
+            user_input['law_price_comp3'] = form.cleaned_data['law_price_comp3']  # 법사가 대비 3
+            user_input['market_price_comp3'] = form.cleaned_data['market_price_comp3']  # 시장가 대비 3
+            user_input['opb_comp3'] = form.cleaned_data['opb_comp3']  # opb 대비 3
+            user_input['submission_date'] = form.cleaned_data['submission_date']  # 법원제출일
+            user_input['next_date'] = form.cleaned_data['next_date']  # 차기기일
+            user_input['fail_count'] = form.cleaned_data['fail_count']  # 유찰회수
 
-            excel_write().save_file(program, opb, property_control_no, interest, setup_price, submission_date)
+            #본건현황
+            user_input['address'] = form.cleaned_data['address']  # Address
+            user_input['property_category'] = form.cleaned_data['property_category']  # Property category
+            user_input['usage'] = form.cleaned_data['usage']  # 용도지역
+            user_input['land_category'] = form.cleaned_data['land_category']  # 지목
+            user_input['state'] = form.cleaned_data['state']  # 이용상황
+            user_input['land_price_m'] = form.cleaned_data['land_price_m']  # 개별공시지가 m
+            user_input['land_price_py'] = form.cleaned_data['land_price_py']  # 개별공시지가 py
+            user_input['land_size_m'] = form.cleaned_data['land_size_m']  # 전체토지면적 m
+            user_input['land_size_py'] = form.cleaned_data['land_size_py']  # 전체토지면적 py
+            user_input['security_size_m'] = form.cleaned_data['security_size_m']  # 담보면적 m
+            user_input['security_size_py'] = form.cleaned_data['security_size_py']  # 담보면적 py
+            user_input['structure'] = form.cleaned_data['structure']  # 건물 구조
+            user_input['permission_date'] = form.cleaned_data['permission_date']  # 사용승인일
+            user_input['floor_usage'] = form.cleaned_data['floor_usage']  # 층별 용도
+            user_input['exclusive_rate'] = form.cleaned_data['exclusive_rate']  # 전용율
+            user_input['exclusive_area_m'] = form.cleaned_data['exclusive_area_m']  # 전유면적 m
+            user_input['exclusive_area_py'] = form.cleaned_data['exclusive_area_py']  # 전유면적 py
+            user_input['contract_area_m'] = form.cleaned_data['contract_area_m']  # 계약면적 m
+            user_input['contract_area_py'] = form.cleaned_data['contract_area_py']  # 계약면적 py
+
+            #건물
+            user_input['building_label']=request.POST.getlist('building_label[]')
+            user_input['building_ho'] = request.POST.getlist('building_ho[]')
+            user_input['building_exclusive_m']=request.POST.getlist('building_exclusive_m[]')
+            user_input['building_exclusive_py'] = request.POST.getlist('building_exclusive_py[]')
+            user_input['building_contract_m'] = request.POST.getlist('building_contract_m[]')
+            user_input['building_contract_py'] = request.POST.getlist('building_contract_py[]')
+            user_input['building_right_m'] = request.POST.getlist('building_right_m[]')
+            user_input['building_right_py'] = request.POST.getlist('building_right_py[]')
+            user_input['building_ratio'] = request.POST.getlist('building_ratio[]')
+            user_input['building_auction_price'] = request.POST.getlist('building_auction_price[]')
+            user_input['building_auction_exclusive'] = request.POST.getlist('building_auction_exclusive[]')
+            user_input['building_auction_contract'] = request.POST.getlist('building_auction_contract[]')
+            user_input['building_auction_ratio'] = request.POST.getlist('building_auction_ratio[]')
+            user_input['building_market_price'] = request.POST.getlist('building_market_price[]')
+            user_input['building_market_exclusive'] = request.POST.getlist('building_market_exclusive[]')
+            user_input['building_market_contract'] = request.POST.getlist('building_market_contract[]')
+            user_input['building_market_ma'] = request.POST.getlist('building_market_ma[]')
+            user_input['building_estimated_price'] = request.POST.getlist('building_estimated_price[]')
+            user_input['building_estimated_exclusive'] = request.POST.getlist('building_estimated_exclusive[]')
+            user_input['building_estimated_contract'] = request.POST.getlist('building_estimated_contract[]')
+            user_input['building_estimated_ea'] = request.POST.getlist('building_estimated_ea[]')
+            user_input['building_estimated_em'] = request.POST.getlist('building_estimated_em[]')
+
+            print(user_input['building_estimated_price'])
+
+            excel_write().save_file(user_input)
 
             BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
             #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -107,5 +178,4 @@ def download(request):
             response=HttpResponse(fh.read(), content_type='application/vnd.ms-excel')
             # 파일이름은 한글로 되어있으면 다운로드를 제공할때 올바르게 제공되지 않는다 - 이유확인해보기
             response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
-            print(os.path.basename(file_path))
             return response
